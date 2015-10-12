b19d3dbeeb24b8fe94eb153a368e42872bb03a6c
Emiliano Corvi <emiliano.corvi@gmail.com>
2015-10-12 16:36:31 -0300

    anda imprimir una matriz

--- src/guiFX/VentanaPrincipal.java
+++ src/guiFX/VentanaPrincipal.java
@@ -13,6 +13,7 @@ import org.rosuda.JRI.Rengine;
 
 import cadenaResponsabilidades.TipoArregloDouble;
 import cadenaResponsabilidades.TipoArregloString;
+import cadenaResponsabilidades.TipoMatrizDouble;
 import cadenaResponsabilidades.TipoObjeto;
 import cadenaResponsabilidades.TipoString;
 import dialogos.Dialogo;
@@ -413,8 +414,7 @@ public class VentanaPrincipal extends BorderPane {
 
 						tablaResultado.getColumns().clear();
 
-						
-						
+				
 						TipoObjeto tipoArregloDouble = new TipoArregloDouble();
 						tipoArregloDouble.ejecutarMetodo(
 								re.eval(((DialogoEjecutar) dialogoEjecutar).getTextFieldNombreDondeDevuelve().getText())
@@ -423,6 +423,14 @@ public class VentanaPrincipal extends BorderPane {
 								((DialogoEjecutar) dialogoEjecutar).getTextFieldNombreDondeDevuelve(),
 								tablaResultado/* areaResultado */);
 
+						TipoObjeto tipoMatrizDouble = new TipoMatrizDouble();
+						tipoMatrizDouble.ejecutarMetodo(
+								re.eval(((DialogoEjecutar) dialogoEjecutar).getTextFieldNombreDondeDevuelve().getText())
+										.asMatrix(),
+								archivo, barraMenu.getListaMetodos(),
+								((DialogoEjecutar) dialogoEjecutar).getTextFieldNombreDondeDevuelve(),
+								tablaResultado/* areaResultado */);
+						
 						TipoObjeto tipoString = new TipoString();
 						tipoString.ejecutarMetodo(
 								re.eval(((DialogoEjecutar) dialogoEjecutar).getTextFieldNombreDondeDevuelve().getText())
